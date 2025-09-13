d04ceb181ff42dcf28bf9d38867fcab44edf16d4
rosmerygomez-netizen <rosmerygomezmartinez7@gmail.com>
2025-09-13 08:31:13 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -3,8 +3,13 @@ $nombre = $_POST['nombre'];
 $edad = $_POST['edad'];
 $direccion = $_POST['direccion'];
 $salario = $_POST['salario'];
+$correo = $_POST['correo'];
+$cargo = $_POST['cargo'];
+
+
 
 echo "<h2>Información recibida</h2>";
 echo "El empleado $nombre tiene $edad años.<br>";
 echo "Vive en $direccion y su salario es: $salario.";
+echo "su correo es $correo y su cargo es: $cargo.";
 ?>
